70dc8a6a732d166d61833d03a56ffa347dded8ce
Ruben Nijhuis <contact@rubennijhuis.com>
2022-05-01 23:27:13 +0200

    base output fix

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rubennijhuis <rubennijhuis@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/13 16:38:00 by rubennijhui   #+#    #+#                 */
-/*   Updated: 2022/03/13 18:09:09 by rubennijhui   ########   odam.nl         */
+/*   Updated: 2022/05/01 23:26:55 by rubennijhui   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 int	main(void)
 {
 	tell_me_a_joke();
-	ft_putstr_fd("\nStandup comedy isn't easy", 1);
+	ft_putendl_fd("Standup comedy isn't easy", 1);
 	return (0);
 }
